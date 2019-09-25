1adbf5a996818703fe5a9f2a2248ee9730dd394c
mini <xwoud>
2019-09-25 16:43:11 +0900

    왜안될까

--- FirstSeoul/FirstSeoul/MileageController.swift
+++ FirstSeoul/FirstSeoul/MileageController.swift
@@ -6,4 +6,55 @@
 //  Copyright © 2019년 김민희. All rights reserved.
 //
 
+import UIKit
 import Foundation
+import WebKit
+
+class MileageController: UIViewController {
+    
+    @IBOutlet var searchBar: UISearchBar!
+    @IBOutlet var webView: WKWebView!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // 첫 페이지는 네이버로 설정
+        self.request(url: "https://www.naver.com")
+    }
+    
+    // 이전 페이지로 이동
+    @IBAction func back() {
+        if self.webView.canGoBack {
+            self.webView.goBack()
+        }
+    }
+    
+    // 다음 페이지로 이동
+    @IBAction func forward() {
+        if self.webView.canGoForward {
+            self.webView.goForward()
+        }
+    }
+    
+    // 현재 webView에서 받아온 URL 페이지를 로드한다.
+    func request(url: String) {
+        self.webView.load(URLRequest(url: URL(string: url)!))
+        self.webView.navigationDelegate = self
+
+    }
+}
+
+// searchBar에서 검색하면 searchBar에 입력된 주소를 요청한다.
+extension MileageController: UISearchBarDelegate {
+    func searchBarSearchButtonClicked(_ searchBar: UISearchBar) {
+        self.request(url: searchBar.text!)
+        //다른곳을 터치하면 키보드가 내려갈 수 있도록 한다.
+        self.view.endEditing(true)
+    }
+}
+
+// 현재 웹페이지의 URL을 searchBar에 띄워준다.
+extension MileageController: WKNavigationDelegate {
+    func webView(_ webView: WKWebView, didFinish navigation: WKNavigation!) {
+        self.searchBar.text = webView.url?.absoluteString
+    }
+}
