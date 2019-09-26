d62be6ce3c758901e5eb16b972d768898fce9a62
mini <xwoud>
2019-09-26 11:08:16 +0900

    로그인까지 완료

--- FirstSeoul/FirstSeoul/ViewController.swift
+++ FirstSeoul/FirstSeoul/ViewController.swift
@@ -8,11 +8,57 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+
+class ViewController: UIViewController,UITextFieldDelegate {
+    
+    @IBOutlet var loginButton: UIButton!
+    @IBOutlet var idField: UITextField!
+    @IBOutlet var pwField: UITextField!
+    
     
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+        loginButton.layer.cornerRadius = 10
+        
+    }
+    
+    @IBAction func login() {
+        guard let idText = idField.text else {
+            return
+        }
+        
+        guard let pwText = pwField.text else {
+            return
+        }
+        
+        if idText == "mini" && pwText == "1234" {
+            successAlert()
+            
+        } else { // 형식은 지켰으나 정의해둔 id(email), password가 아닌 경우
+            failedAlert()
+        }
+        
+        
+    }
+    
+    
+    // alert 설정
+    func failedAlert() {
+        let alertController = UIAlertController(title: nil, message: "아이디와 비번을 확인해주세요.", preferredStyle: .alert)
+        alertController.addAction(UIAlertAction(title: "확인", style: .default, handler: nil))
+        self.present(alertController, animated: true, completion: nil)
+    }
+    
+    func successAlert() {
+        let alertController = UIAlertController(title: nil, message: "로그인에 성공하였습니다!", preferredStyle: .alert)
+        alertController.addAction(UIAlertAction(title: "확인", style: .default, handler: nil))
+        //self.present(alertController, animated: true, completion: nil)
+    }
+    
+    func joinAlert() {
+        let alertController = UIAlertController(title: nil, message: "입력하신 내용으로 회원가입을 해주세요!", preferredStyle: .alert)
+        alertController.addAction(UIAlertAction(title: "확인", style: .default, handler: nil))
+        self.present(alertController, animated: true, completion: nil)
     }
     
     
