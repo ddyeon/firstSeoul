1e296f1e79efaa22b8eb26aa0aac78b5ee226681
mini <xwoud>
2019-09-26 09:35:08 +0900

    마일리지는 끗

--- FirstSeoul/FirstSeoul/Milege2Controller.swift
+++ FirstSeoul/FirstSeoul/Milege2Controller.swift
@@ -19,7 +19,7 @@ class Mileage2Controller: UIViewController {
     
     override func viewDidLoad() {
         super.viewDidLoad()
-        self.request(url: "https://driving-mileage.seoul.go.kr/home/mileage/howtomobile.do?menuNo=1016")
+        self.request(url: "https://driving-mileage.seoul.go.kr/home/mileage/howtoetax.do?menuNo=1016")
     }
     
     
@@ -31,3 +31,19 @@ class Mileage2Controller: UIViewController {
         
     }
 }
+// searchBar에서 검색하면 searchBar에 입력된 주소를 요청한다.
+extension Mileage2Controller: UISearchBarDelegate {
+    func searchBarSearchButtonClicked(_ searchBar: UISearchBar) {
+        self.request(url: searchBar.text!)
+        //다른곳을 터치하면 키보드가 내려갈 수 있도록 한다.
+        self.view.endEditing(true)
+    }
+}
+
+// 현재 웹페이지의 URL을 searchBar에 띄워준다.
+extension Mileage2Controller: WKNavigationDelegate {
+    func webView(_ webView: WKWebView, didFinish navigation: WKNavigation!) {
+        self.searchBar.text = webView.url?.absoluteString
+    }
+}
+
